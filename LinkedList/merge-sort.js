029b090f2560bb13e1912075daa2b40ad37035f8
Cameron <mooreheadc@hotmail.com>
2017-11-02 21:19:36 -0700

    merge-sort for linkedlists added

--- LinkedList/merge-sort.js
+++ LinkedList/merge-sort.js
@@ -2,6 +2,60 @@
 // (in ascending order) using merge sort and return new
 // head pointer of sorted linked list
 
-const mergeSortLL = head => {
-  
+const mergeSort = head => {
+  if (!head || !head.next) {
+    return head
+  }
+
+  let first = null
+  let second = null
+  let firstSecond = { first, second }
+
+  split(head, firstSecond)
+
+  firstSecond.first = mergeSort(firstSecond.first)
+  firstSecond.second = mergeSort(firstSecond.second)
+
+  return mergeParts(firstSecond.first, firstSecond.second)
 }
+
+const split = (head, firstSecond) => {
+  if (!head) {
+    firstSecond.first = null
+    firstSecond.second = null
+    return;
+  }
+
+  if (!head.next) {
+    firstSecond.first = head
+    firstSecond.second = null
+  } else {
+    let slow = head
+    let fast = head.next
+    while (fast) {
+      fast = fast.next
+      if (fast) {
+        fast = fast.next
+        slow = slow.next
+      }
+    }
+
+    firstSecond.first = head
+    firstSecond.second = slow.next
+
+    slow.next = null
+  }
+}
+
+let test = {
+  data: 5,
+  next: {
+    data: 2,
+    next: {
+      data: 3,
+      next: null
+    }
+  }
+}
+
+console.log(mergeSort(test))
