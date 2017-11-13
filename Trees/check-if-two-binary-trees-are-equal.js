af7782d23ad49adbb693df933e8cb49ff6085747
Cameron <mooreheadc@hotmail.com>
2017-11-12 16:25:31 -0800

    identical binary trees

---
+++ Trees/check-if-two-binary-trees-are-equal.js
@@ -0,0 +1,16 @@
+// Given roots of two binary trees, determine if these trees are
+// identical or not.
+
+const areIdentical = (root1, root2) => {
+  if (!root1 && !root2) {
+    return true
+  }
+
+  if (root1 && root2) {
+    return (root1.data === root2.data &&
+            areIdentical(root1.left, root2.left) &&
+            areIdentical(root1.right, root2.right))
+  }
+
+  return false
+}
